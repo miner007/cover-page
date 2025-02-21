b668db1dcc1928c0ade956abb5df57f4aa51bc30
miner007 <alisdair123456@gmail.com>
2025-02-21 10:19:21 +0800

    Update 0.0.1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // This tells Next.js to generate a fully static export.
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/cover-page",
+  assetPrefix: "/cover-page",
+  // any other config options...
 };
 
 export default nextConfig;
